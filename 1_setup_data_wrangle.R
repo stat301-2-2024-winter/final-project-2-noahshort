63660234d3cff0d24d91125085c89e1826290eec
Noah Short <noahshort2025@u.northwestern.edu>
2024-02-28 21:25:28 -0600

    Folds complete

--- 1_setup_data_wrangle.R
+++ 1_setup_data_wrangle.R
@@ -8,32 +8,23 @@ bio_data <- read_csv("data/all_seasons.csv")
 
 #Need season in bio_data to only be the 2nd year of the season
 
-bio_data |> 
-  mutate(
-    season = substr(season, 6, 7)
-  )
 
 statistics_data_clean <- statistics_data |> 
-  mutate(
-    season = substr(season, 3, 4)
+  filter(
+    season != 2022
   )
 
 #Turning draft round into a factor, making it and draft pick uniform when undrafted
+# Also creating factor for home country
 
-bio_data |> 
-  mutate(
-    draft_round = if_else(draft_round == 1 | draft_round == 2,
-                          true = draft_round,
-                          false = "U")
-  ) |> 
-  mutate(
-    draft_round = factor(
-      draft_round, levels = c(1, 2, "U")
-    ),
-    draft_number = as.numeric(draft_number)
-  )
 
-bio_data_clean <- bio_data |> 
+bio_data_clean <- bio_data |>
+  mutate(
+    season = if_else(season == "1999-00",
+                     true = "2000",
+      false = paste0(substr(season, 1, 2), substr(season, 6, 7))
+  )) |> 
+  filter(season != 2022) |> 
   mutate(
     draft_round = if_else(draft_round == 1 | draft_round == 2,
                           true = draft_round,
@@ -41,7 +32,6 @@ bio_data_clean <- bio_data |>
     draft_round = factor(
       draft_round, levels = c(1, 2, "U")
     ),
-    season = substr(season, 6, 7),
     draft_number = as.numeric(draft_number),
     country = if_else(country == "USA",
                       true = "USA",
@@ -59,29 +49,33 @@ joined_data <- inner_join(
   rename(season = season.x) |> 
   select(!season.y)
 
-predictor_data <- joined_data |> 
+basketball_data <- joined_data |> 
   arrange(player_name, season) |> 
   group_by(player_name) |> 
   mutate(next_season_war_total = lead(war_total, default = NA)) |> 
   relocate(war_total, next_season_war_total) |> 
   ungroup()
 
-predictor_data |> 
+basketball_data |> 
   group_by(season) |> 
   summarize(n = n()) |> print(n = 26)
 #should get the war values for 2023
 
-predictor_data |> 
-  filter(season != 22) |> 
+basketball_data |> 
   naniar::miss_var_summary()
 #outcome variable is only missing 15.8% of time for seasons outside of most recent
 #getting the 2023 war data would reduce outcome missingness by more than 4 pps.
 
-missing_report_predictor <- predictor_data |> naniar::miss_var_summary() |> knitr::kable()
+missing_report_predictor <- basketball_data |> naniar::miss_var_summary() |> knitr::kable()
 save(missing_report_predictor, file = here("results/missing_report_predictor.rda"))
 
-write_csv(predictor_data, "data/complete_data_fp2.csv")
+write_csv(basketball_data, "data/complete_data_fp2.csv")
 
-predictor_data |> skimr::skim_without_charts(next_season_war_total)
+save(basketball_data, file = here("data/basketball_data.rda"))
 
+basketball_data |> skimr::skim_without_charts(next_season_war_total)
+
+basketball_data |> 
+  ggplot(aes(next_season_war_total)) +
+  geom_histogram()
 
---
+++ 2_data_splits_resamples.R
@@ -0,0 +1,37 @@
+#load packages
+library(tidyverse)
+library(tidymodels)
+library(here)
+library(doMC)
+
+#prefer tidymodels
+tidymodels_prefer()
+
+#load in data 
+
+load("data/basketball_data.rda")
+
+basketball_train <- basketball_data |> 
+  filter(season != 18 | season != 19 | season != 20 | season != 21)
+basketball_test <- basketball_data |> 
+  filter(season == 18 | season == 19 | season == 20 | season == 21)
+
+save(basketball_train, file = here("data/basketball_train.rda"))  
+save(basketball_test, file = here("data/basketball_test.rda"))
+
+keep_pred <- control_resamples(save_pred = TRUE, save_workflow = TRUE)
+
+save(keep_pred, file = here("data/keep_pred.rda"))
+
+basketball_train |> summarize(
+  n = n(),
+  .by = season
+)
+
+basketball_folds <- vfold_cv(
+  basketball_train, 
+  v = 10, 
+  repeats = 4, 
+  strata = next_season_war_total)
+
+save(basketball_folds, file = here("data/basketball_folds.rda"))
