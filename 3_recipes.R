3ccd71a858318c24103ed045311e2c41768ad840
Noah Short <noahshort2025@u.northwestern.edu>
2024-03-07 13:22:18 -0600

    Recipes created

--- 3_recipes.R
+++ 3_recipes.R
@@ -35,6 +35,93 @@ basketball_recipe_memo |>
 save(basketball_recipe_memo, file = here("recipes/basketball_recipe_memo.rda"))
 
 
+# Baseline Recipe - simply uses variables used in both types of recipes
 
+basketball_recipe_baseline <- recipe(
+  next_season_war_total ~
+    war_total + pts + reb + ast + draft_round,
+  data = basketball_train
+) |> 
+  step_dummy(draft_round) |> 
+  step_zv(all_predictors()) |> 
+  step_normalize(all_numeric_predictors())
 
+basketball_recipe_baseline |> 
+  prep() |> 
+  bake(new_data = NULL) |> 
+  glimpse()
+
+save(basketball_recipe_baseline, file = here("recipes/basketball_recipe_baseline.rda")) 
+
+basketball_recipe_lm_stats <- recipe(
+  next_season_war_total ~
+    war_total + raptor_offense + raptor_defense + pts + reb + ast + draft_round,
+  data = basketball_train
+) |> 
+  step_dummy(draft_round) |> 
+  step_interact(~ pts:raptor_offense) |> 
+  step_interact(~ ast:raptor_offense) |> 
+  step_zv(all_predictors()) |> 
+  step_normalize(all_numeric_predictors())
+
+basketball_recipe_lm_stats |> 
+  prep() |> 
+  bake(new_data = NULL) |> 
+  glimpse()
+
+save(basketball_recipe_lm_stats, file = here("recipes/basketball_recipe_lm_stats.rda"))
+
+basketball_recipe_rf_stats <- recipe(
+  next_season_war_total ~
+    war_total + raptor_offense + raptor_defense + pts + reb + ast + draft_round,
+  data = basketball_train
+) |> 
+  step_dummy(draft_round, one_hot = TRUE) |> 
+  step_interact(~ pts:raptor_offense) |> 
+  step_interact(~ ast:raptor_offense) |> 
+  step_zv(all_predictors()) |> 
+  step_normalize(all_numeric_predictors())
+
+basketball_recipe_rf_stats |> 
+  prep() |> 
+  bake(new_data = NULL) |> 
+  glimpse()
+
+save(basketball_recipe_rf_stats, file = here("recipes/basketball_recipe_rf_stats.rda"))
+
+basketball_recipe_lm_bio <- recipe(
+  next_season_war_total ~
+    war_total + pts + reb + ast + age + player_height + player_weight + draft_round + country,
+  data = basketball_train
+) |> 
+  step_dummy(draft_round) |> 
+  step_dummy(country) |> 
+  step_interact(~ player_height:player_weight) |> 
+  step_zv(all_predictors()) |> 
+  step_normalize(all_numeric_predictors())
+
+basketball_recipe_lm_bio |> 
+  prep() |> 
+  bake(new_data = NULL) |> 
+  glimpse()
+
+save(basketball_recipe_lm_bio, file = here("recipes/basketball_recipe_lm_bio.rda"))
+
+basketball_recipe_rf_bio <- recipe(
+  next_season_war_total ~
+    war_total + pts + reb + ast + age + player_height + player_weight + draft_round + country,
+  data = basketball_train
+) |> 
+  step_dummy(draft_round, one_hot = TRUE) |> 
+  step_dummy(country, one_hot = TRUE) |> 
+  step_interact(~ player_height:player_weight) |> 
+  step_zv(all_predictors()) |> 
+  step_normalize(all_numeric_predictors())
+
+basketball_recipe_rf_bio |> 
+  prep() |> 
+  bake(new_data = NULL) |> 
+  glimpse()
+
+save(basketball_recipe_rf_bio, file = here("recipes/basketball_recipe_rf_bio.rda"))
 
